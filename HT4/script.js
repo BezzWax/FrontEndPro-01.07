15139e8134d1e1a0e1d75da5261a7f4e01f9d739
Eugene <87862942+BezzWax@users.noreply.github.com>
2023-07-11 15:27:42 +0300

    Delete script.js

--- HT4/script.js
+++
@@ -1,6 +0,0 @@
-let hours = prompt('Hours:');
-let minutes = prompt('Minutes:');
-let seconds = prompt('Seconds:');
-let time = (hours * 60 + minutes) * 60 + seconds;
-
-alert(`In ${hours} hours and ${minutes} minutes and ${seconds} seconds is ${time}`);
