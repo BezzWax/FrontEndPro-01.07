2978655d0be2ff75fa6155f878235de954d9037a
Beezwax <kolomiec.evgeniy03@gmail.com>
2023-07-11 15:43:18 +0300

    New commit

--- HT4/script.js
+++ HT4/script.js
@@ -1,6 +1,7 @@
-let hours = Number(prompt('Hours:'));
-let minutes = Number(prompt('Minutes:'));
-let seconds = Number(prompt('Seconds:'));
+let hours = Number(prompt('Hours: '));
+let minutes = Number(prompt('Minutes: '));
+let seconds = Number(prompt('Seconds: '));
+
 let time = (hours * 60 + minutes) * 60 + seconds;
 
-alert(`In ${hours} hours and ${minutes} minutes and ${seconds} seconds is ${time}`);
+alert(time);
