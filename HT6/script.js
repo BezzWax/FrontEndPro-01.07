c28664bfae8dea9628e6063c93a1e4fe9cdba35b
Beezwax <kolomiec.evgeniy03@gmail.com>
2023-07-15 14:41:27 +0300

    Task 6

---
+++ HT6/script.js
@@ -0,0 +1,22 @@
+let math = prompt("Enter a math action (+, -, *, /): ");
+
+let num1 = +prompt("Enter a first number");
+let num2 = +prompt("Enter a second number");
+
+switch(math){
+    case "+":
+        alert(num1 + num2);
+        break;
+    case "-":
+        alert(num1 - num2);
+        break;
+    case "*":
+        alert(num1 * num2);
+        break;
+    case "/":
+        alert(num1 / num2);
+    break;
+    default:
+        alert("Invalid math action");
+}
+    
