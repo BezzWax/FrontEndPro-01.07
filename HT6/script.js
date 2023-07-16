18a099cb9f7dd87eb387632cf088fedd85a92ca7
Eugene <87862942+BezzWax@users.noreply.github.com>
2023-07-16 09:32:10 +0300

    Update script.js

--- HT6/script.js
+++ HT6/script.js
@@ -5,18 +5,19 @@ let num2 = +prompt("Enter a second number");
 
 switch(math){
     case "+":
-        alert(num1 + num2);
+        alert(`${num1} ${math} ${num2} = ${num1 + num2}`);
         break;
     case "-":
-        alert(num1 - num2);
+        alert(`${num1} ${math} ${num2} = ${num1 - num2}`);
         break;
     case "*":
-        alert(num1 * num2);
+        alert(`${num1} ${math} ${num2} = ${num1 * num2}`);
         break;
     case "/":
-        alert(num1 / num2);
+        alert(`${num1} ${math} ${num2} = ${num1 / num2}`);
     break;
     default:
         alert("Invalid math action");
-}
+};
+   
     
