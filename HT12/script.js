f0209a4964bf0ab01da89c53a7d7fc9fe3132b1b
Beezwax <kolomiec.evgeniy03@gmail.com>
2023-07-25 12:30:25 +0300

    Modify script.js

--- HT12/script.js
+++ HT12/script.js
@@ -1,23 +1,34 @@
 
-function removeElement() {
-    const len = prompt('Array lenght');
-    let array = [];
+// function removeElement() {
+//     const len = prompt('Array lenght');
+//     let array = [];
 
-    for(let i = 0; i < len; i++) {
-        let temp = prompt('Input element');
-        array.push(temp);
+//     for(let i = 0; i < len; i++) {
+//         let temp = prompt('Input element');
+//         array.push(temp);
+//     }
+
+//     let delElement = prompt(`${array}, which element to remove?`);
+
+//     for(let element of array){ // delete element by value
+//         if(element == delElement){
+//             let index = array.indexOf(delElement);
+//             array.splice(index, 1);
+//         }
+//     }
+
+//     console.log(array);
+// }
+
+// removeElement();
+
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+    if (index !== -1) {
+      array.splice(index, 1);
     }
-
-    let delElement = prompt(`${array}, which element to remove?`);
-
-    for(let element of array){ // delete element by value
-        if(element == delElement){
-            let index = array.indexOf(delElement);
-            array.splice(index, 1);
-        }
-    }
-
-    console.log(array);
-}
-
-removeElement();
+  }
+  
+  const array = [1, 2, 3, 4, 5, 6, 7];
+  removeElement(array, 5);
+  console.log(array);
