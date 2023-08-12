28fc228deba986d27c9254fe68b5dea3af8bbdf2
Beezwax <kolomiec.evgeniy03@gmail.com>
2023-08-12 10:50:08 +0300

    Task 18: OOP

---
+++ HT18/script.js
@@ -0,0 +1,45 @@
+// 1. 
+
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    humanShowInfo() {
+        console.log(this.name + ": " + this.age);
+    }
+};
+
+
+//2
+
+class Car {
+    constructor(brand, model, yearOfProduction, carNumber, owner){
+        this.brand = brand;
+        this.model = model;
+        this.yearOfProduction = yearOfProduction;
+        this.carNumber = carNumber;
+
+        if(owner.age >= 18){
+            this.owner = owner;
+        } else {
+            console.log('Owner can not use the car');
+        }
+    }
+
+    showInfo() {
+        console.log(`Car: ${this.brand} ${this.model}, Year: ${this.yearOfProduction}, License Plate: ${this.carNumber}`);
+        this.owner.humanShowInfo();
+    }
+
+}
+
+const human1 = new Human("John", 25);
+const human2 = new Human("Alice", 67);
+
+const car1 = new Car("Toyota", "Camry", 2022, "ABC123", human1);
+const car2 = new Car("Ford", "Mustang", 2018, "XYZ789", human2);
+
+car1.showInfo();
+car2.showInfo();
