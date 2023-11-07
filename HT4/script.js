8191a7dcb3048068481aaa7976d255ebac88011c
Eugene <87862942+BezzWax@users.noreply.github.com>
2023-11-07 16:37:20 +0200

    Delete HT4 directory

--- HT4/script.js
+++
@@ -1,7 +0,0 @@
-let hours = Number(prompt('Hours: '));
-let minutes = Number(prompt('Minutes: '));
-let seconds = Number(prompt('Seconds: '));
-
-let time = (hours * 60 + minutes) * 60 + seconds;
-
-alert(time);
