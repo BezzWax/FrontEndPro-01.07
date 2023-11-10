5e7ab9bd54918b6181f4dc22706bdab2f9fa2675
Eugene <87862942+BezzWax@users.noreply.github.com>
2023-11-10 18:05:30 +0200

    Task 35: Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify-html', () => {
 	return gulp.src('index.html')
@@ -14,10 +15,17 @@ gulp.task('minify-js', () => {
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('build', gulp.parallel('minify-html', 'minify-js'));
+gulp.task('minify-css', () => {
+	return gulp.src('style.css')
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('build'));
+})
+
+gulp.task('build', gulp.parallel('minify-html', 'minify-css', 'minify-js'));
 
 gulp.task('watch', () => {
 	gulp.watch('index.html', gulp.series('minify-html'));
+	gulp.watch('style.css', gulp.series('minify-css'));
 	gulp.watch('script.js', gulp.series('minify-js'));
 });
 
